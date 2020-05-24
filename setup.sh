d8c521e51fe449b798f095c4d04575fae4d5204b
Scott Wolford <sawolford@gmail.com>
2020-05-23 21:18:22 -0400

    revamped how .gitconfig is created and made platform-specific configurations
    added cmd,gitbash,linux,macos,unix,windows,wsl
    removed gitconfig,template
    moved gitconfig to all
    miscellaneous cleanup

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+    echo Usage: $0 \"name\" \"email\"
+    exit 1
+fi
+name=$1
+email=$2
+path=$(pwd)
+relpath=${path/${HOME}\//}
+ofile=${HOME}/.gitconfig
+[ -e $ofile ] && mv -f $ofile $ofile.bak
+echo Writing to $ofile:
+echo "[user]" | tee -a $ofile
+echo "	name = $name" | tee -a $ofile
+echo "	email = $email" | tee -a $ofile
+echo "[include]" | tee -a $ofile
+echo "	path = $relpath/all" | tee -a $ofile
+unames=$(uname -s)
+if [[ ${unames} =~ ^Darwin ]]; then platform=macos
+elif [[ $TERM = cygwin ]]; then platform=cmd
+elif [[ ${unames} =~ ^MINGW ]]; then platform=gitbash
+elif env | grep ^WSL_ >/dev/null; then platform=wsl
+else platform=linux
+fi
+echo "	path = $relpath/$platform" | tee -a $ofile
+echo "	path = $relpath/local" | tee -a $ofile
